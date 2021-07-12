227262766ea1974ab8cf748b535336fb19caa64c
Cristiano Moraes Bilacchi Azarias <cristiano@Cristiano.local>
2021-07-12 15:51:21 -0300

    fixing names

--- cutter.py
+++
@@ -1,8 +0,0 @@
-"""Drag the sphere to cut the mesh interactively
-Use mouse buttons to zoom and pan"""
-from vedo import *
-
-s = Mesh('mesh/brenda2.ply')
-
-plt = show(s, __doc__, bg='black', bg2='bb', interactive=False)
-plt.addCutterTool(s) #modes= sphere, plane, box
