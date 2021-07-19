9bfe13011e9b5286618fc490d587cd4ff8a53be1
Cristiano Moraes Bilacchi Azarias <cristiano@Cristiano.local>
2021-07-19 00:52:14 -0300

    Moving scripts to new folder

---
+++ FrontEnd/jsonExample.py
@@ -0,0 +1,18 @@
+import json
+from datetime import datetime, timedelta
+import numpy as np
+
+n = 10
+dates = [(datetime.now()-timedelta(days=7*i)).strftime('%y%m%d') for i in range(n)]
+path = ['../mesh/brenda2.ply', '../mesh/brenda4.ply']
+
+
+jsonstr = {
+    "Gordura":{
+        "date":dates,
+        "file":np.random.choice(path, n).tolist()
+    }
+}
+
+with open('timeline.json', 'w') as jsonf:
+    json.dump(jsonstr, jsonf)
